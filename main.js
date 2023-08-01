245609a6ba1802b7207202a1a917b0314b506a1e
DarienMS <cabotastico@gmail.com>
2023-08-01 19:18:37 -0300

    Segunda preentrega

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-function calcularPagosEnCuotas() {
+ /* function calcularPagosEnCuotas() {
     let continuar = true
   
     while (continuar) {
@@ -26,4 +26,80 @@ function calcularPagosEnCuotas() {
     }
   }
   
-  calcularPagosEnCuotas();
+  calcularPagosEnCuotas(); */ 
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////* Preentrega 2 *///////////////////////////////////////////////////////////
+  
+
+
+
+
+
+
+  class Producto {
+    constructor(nombre, precio, stock) {
+      this.nombre = nombre;
+      this.precio = precio;
+      this.stock = stock;
+    }
+  }
+  
+  const productos = [];
+  
+  function agregarProducto() {
+    const nombre = prompt("Ingrese el nombre del producto");
+    const precio = parseFloat(prompt("Ingrese el precio del producto"));
+    const stock = parseInt(prompt("Ingrese la cantidad del producto"));
+  
+    if (isNaN(precio) || isNaN(stock)) {
+      console.log("Error: El precio y el stock deben ser valores validos");
+      return;
+    }
+  
+    const producto = new Producto(nombre, precio, stock);
+    productos.push(producto);
+    console.log("Producto agregado");
+  }
+  
+  function eliminarProducto() {
+    const nombre = prompt("Ingrese el nombre del producto que desea eliminar");
+    const index = productos.findIndex(producto => producto.nombre === nombre);
+  
+    if (index !== -1) {
+      productos.splice(index, 1);
+      console.log("Producto eliminado exitosamente");
+    } else {
+      console.log("Error: No se encontró un producto con ese nombre");
+    }
+  }
+  
+  function buscarProducto() {
+    const nombre = prompt("Ingrese el nombre del producto que desea buscar");
+    const producto = productos.find(producto => producto.nombre === nombre);
+  
+    if (producto) {
+      console.log("Producto encontrado");
+      console.log(producto);
+    } else {
+      console.log("Error: No se encontro un producto con ese nombre");
+    }
+  }
+  
+  
+  
+ 
+  agregarProducto();
+  agregarProducto();
+  agregarProducto();
+  console.log(productos);
+  
+  eliminarProducto();
+  console.log(productos);
+  
+  buscarProducto();
+  
+ 
