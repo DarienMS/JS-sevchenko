f12f6aa3575359bcb92b1b0b4889dfaf9d8151bc
DarienMS <cabotastico@gmail.com>
2023-08-15 19:49:31 -0300

    PE-3

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Lomo",
+    precio: 500,
+    img:
+    "./img/lomo.webp",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Hamburguesa 1",
+    precio: 450,
+    img:
+    "./img/hamburguesa-completa.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Hamburguesa 2",
+    precio: 500,
+    img:
+    "./img/hamburguesa.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Pasta",
+    precio: 800,
+    img:
+    "./img/pasta.jpeg",
+    cantidad: 1,
+  },
+];
